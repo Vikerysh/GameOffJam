f42a8512fad9099e0836aa290179846dcc19af4a
Vikerysh <93541634+Vikerysh@users.noreply.github.com>
2021-11-24 10:21:19 -0500

    Background

--- New Unity ProjectGameJam/Assets/Scripts/WorldScripts/Background.cs
+++ New Unity ProjectGameJam/Assets/Scripts/WorldScripts/Background.cs
@@ -6,13 +6,15 @@ public class Background : MonoBehaviour
 {
 
     GameObject cam;
-    float length, startpos;
+    float length, startpos, startposY;
     public float parallaxEffect;
+    public float parallaxEffectY;
     // Start is called before the first frame update
     void Start()
     {
         cam = GameController.instance.cam.gameObject;
         startpos = transform.position.x;
+        startposY = transform.position.y;
         length = GetComponent<SpriteRenderer>().bounds.size.x;
     }
 
@@ -21,8 +23,9 @@ public class Background : MonoBehaviour
     {
         float temp = (cam.transform.position.x * (1 - parallaxEffect));
         float dist = (cam.transform.position.x * parallaxEffect);
+        float distY = (cam.transform.position.y * parallaxEffectY);
 
-        transform.position = new Vector3(startpos + dist, transform.position.y, transform.position.z);
+        transform.position = new Vector3(startpos + dist, startposY + distY, transform.position.z);
 
         if(temp > startpos + length) startpos += length;
         else if(temp < startpos - length) startpos -= length;
