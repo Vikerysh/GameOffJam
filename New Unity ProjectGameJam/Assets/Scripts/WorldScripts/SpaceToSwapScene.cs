41c713d96190c62bdf1ae772a94d731f3f8de12b
Vikerysh <93541634+Vikerysh@users.noreply.github.com>
2021-11-30 23:31:36 -0500

    Adding Text Start to game

---
+++ New Unity ProjectGameJam/Assets/Scripts/WorldScripts/SpaceToSwapScene.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SpaceToSwapScene : MonoBehaviour
+{
+    [SerializeField]
+    private string levelToLoad; 
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if( Input.GetButtonDown("Jump")){
+            SceneManager.LoadScene(levelToLoad);
+        }
+    }
+}
